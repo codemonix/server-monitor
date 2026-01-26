11fea0d19a6060d435e17fe551da933e3ed68cad
Saeid Monfared <s_monfared1@yahoo.com>
2026-01-26 21:08:51 +0300

    fix: add debug log to docker-entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+
 echo "Generating Runtime Config..."
+
+echo "DEBUG: API_BASE_URL is set to: '$API_BASE_URL'"
+echo "DEBUG: WS_BASE_URL is set to: '$WS_BASE_URL'"
+
+
 envsubst < /usr/share/nginx/html/runtime-config.js.tpl > /usr/share/nginx/html/config.js
 
 echo "Starting Nginx..."
