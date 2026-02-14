462096321ed777ef78b684f476a57f8e251490f5
Saeid Monfared <s_monfared1@yahoo.com>
2026-02-14 17:55:52 +0300

    fix: config issue

--- frontend/public/config.js
+++ frontend/public/config.js
@@ -1,6 +1,5 @@
 
 window.config = window.config || {
     API_BASE_URL: 'http://localhost:4000',
-    WS_BASE_URL: 'http://localhost:4000/ws',
-    IS_DEMO: true
+    WS_BASE_URL: 'http://localhost:4000/ws'
 }
--- frontend/runtime-config.js.tpl
+++ frontend/runtime-config.js.tpl
@@ -1,5 +1,4 @@
 window.config = {
     API_BASE_URL: "${API_BASE_URL}",
     WS_BASE_URL: "${WS_BASE_URL}",
-    IS_DEMO: ${IS_DEMO},
 };
